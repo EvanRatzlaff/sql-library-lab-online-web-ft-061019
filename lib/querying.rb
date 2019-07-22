fa11742e8ed65c0339d7a564312ae219eb0ab90a
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-22 16:30:09 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT name, COUNT(series_id) FROM characters"
+  "SELECT COUNT(name), COUNT(series_id) FROM characters"
 end
