038b05aab6c441eba0e812644f9a11e9d40ba0a2
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-22 16:18:05 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT characters.series_id, MAX(characters) FROM series JOIN characters ON characters.series_id = series.id"
+  "SELECT characters.series_id, COUNT(characters) FROM series JOIN characters ON characters.series_id = series.id GROUP BY species ORDER BY DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
